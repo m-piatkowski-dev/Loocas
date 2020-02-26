dda11d4aa73ac724fd2171b5d82a4039698cc71f
@misha666 <mishaoa@gmail.com>
2020-02-26 21:03:17 +0100

    release oldone

--- projekt_release/functionPerson.cpp
+++ projekt_release/functionPerson.cpp
@@ -20,19 +20,21 @@ Person::Person( const std::string& _name,
              int _licznik):
                 name(_name), surname(_surname), address(_address),
                  age(_age), pesel(_pesel), date_of_birth(_date_of_birth), licznik(_licznik){}  
-
-                 std::ostream& operator<<(std::ostream& s,const Person & p)
+             
+std::ostream& operator<<(std::ostream& s,const Person & p)
     {
         s <<std::setw(10)<<std::left<<p.get_name()<<" "<<std::setw(18)<<std::left<<p.get_surname()<<" "<<std::setw(20)<<std::left<<p.get_address()<<" "
           <<std::setw(4)<<std::left<<p.get_age()<<" "<<std::setw(15)<<std::left<<p.get_pesel()<<std::setw(15)<<std::left<<p.get_date_of_birth()
           <<std::setw(12)<<std::left<<"licznik: "<<std::left<<p.get_licznik()<<"."<<std::endl;
         return s;
     }
-    std::ostream& operator<<(std::ostream& r, std::pair<const std::string, std::shared_ptr<Person>> p)
+
+std::ostream& operator<<(std::ostream& r, std::pair<const std::string, std::shared_ptr<Person>> p)
     {
         r <<"Pesel->"<<p.first<<" "<<*p.second<<std::endl;
         return r;
     }
+
 Person Person::get_random_person()
     {
         const std::array<std::string, 10> imiona{"Anna", "Mariola", "Stefan", "Barbara", "Michal",
@@ -45,26 +47,41 @@ Person Person::get_random_person()
                                                      "Lialiowski", "Jajowski", "Swieczkowski", "Glupkowski", "Ossowski" };
         const std::array<std::string, 10> adres{"Szenwalda", "Wojska Polskiego", "Niemierzynska", "Mickiewicza", "Plac Rodla",
                                                 "Plac Grunwaldzki", "Krzywoustego", "Konopnickiej", "Traugutta", "Poniatowskiego"};
-        int year = std::rand()%120+1900;
-        std::string month = std::to_string(std::rand()%12+1);
-        if( std::stoi(month)<10 ){ month = "0" + month; }
-        std::string  day = std::to_string(std::rand()%31+1);
-        if( (std::stoi(month)== 4 || 6 || 9 || 11 )){day = std::to_string(std::rand()%30+1);}
-        else if( std::stoi(month)==2 ){ day = std::to_string(std::rand()%28+1); }
-        else if((std::stoi(month)==2)&&( ((year%4==0)&&(year%100!=0)) or (year%400==0) ) ){ day = std::to_string(std::rand()%29+1); }
-        if( std::stoi(day)<10 ){ day = "0" + day;}
-        std::string nnnnn = std::to_string(std::rand()%90000+9999);
         
+        int year_in = std::rand()%120+1900;
+        int month_in = std::rand()%12+1; 
+        int day_in;
+
+        if((month_in==2)&&( ((year_in%4==0)&&(year_in%100!=0)) || (year_in%400==0) ) ){ day_in = std::rand()%29+1; }
+        else if ( month_in==2 ){ day_in = std::rand()%28+1;}
+        else if( (month_in == 4 || month_in == 6 || month_in == 9 || month_in == 11 )){day_in = std::rand()%30+1;}
+        else  day_in = std::rand()%31+1; 
+
         std::string name = imiona[std::rand()%10];
         std::string surname = nazwiska[std::rand()%30];
         std::string address = ( adres[std::rand()%10]) + " " + std::to_string(std::rand()%98+1 );
-        int age = 2020-year;
+
+        int age = 2020-year_in;
+
+        std::string month;
+        std::string day; 
+        if( month_in<10 ){ month = "0" + std::to_string(month_in);} else { month = std::to_string(month_in); }
+        if( day_in<10 ){ day = "0" + std::to_string(day_in);} else { day = std::to_string(day_in);}
+        std::string nnnnn;
+        int random = std::rand()%99999;
+        if(random<10){ nnnnn = "0000" + std::to_string(random);}
+        else if(random<100){ nnnnn = "000" + std::to_string(random);}
+        else if(random<1000){ nnnnn = "00" + std::to_string(random);}
+        else if(random<10000){ nnnnn = "0" + std::to_string(random);}
+        else nnnnn = std::to_string(random);
+
         std::string pesel;
-        if( year>=2010){ pesel = std::to_string(year-2000) + std::to_string(std::stoi(month)+20) + day + nnnnn; }
-        else if( (year>=2000)&&(year<2010) ){ pesel = "0" + std::to_string(year-2000) + std::to_string(std::stoi(month)+20) + day + nnnnn ;}
-        else if( (year>=1900)&&(year<1910) ) { pesel = "0" + std::to_string(year-1900) + month + day + nnnnn;}
-        else { pesel = std::to_string(year-1900) + month + day + nnnnn;}
-        std::string date_of_birth =std::to_string(year) + "." + month + "." + day;
+
+        if( year_in>=2010 ){ pesel = std::to_string(year_in-2000) + std::to_string(std::stoi(month)+20) + day + nnnnn; }
+        else if( (year_in>=2000)&&(year_in<2010) ){ pesel = "0" + std::to_string(year_in-2000) + std::to_string(std::stoi(month)+20) + day + nnnnn ;}
+        else if( (year_in>=1900)&&(year_in<1910) ) { pesel = "0" + std::to_string(year_in-1900) + month + day + nnnnn;}
+        else { pesel = std::to_string(year_in-1900) + month + day + nnnnn;}
+        std::string date_of_birth =std::to_string(year_in) + "." + month + "." + day;
        
         return Person(
             name,
--- projekt_release/projekt.cpp
+++ projekt_release/projekt.cpp
@@ -12,21 +12,77 @@
    
 int main(int argc, char* argv[])
 {
-    std::map<std::string, std::shared_ptr<Person>> baza;
     std::srand(time(nullptr));
-    Person s;
+    std::map<std::string, std::shared_ptr<Person>> baza;
     
-    for( int i=0; i<std::stoi(argv[1]); ++i)
-    { 
-        std::shared_ptr<Person> p=std::make_shared<Person>(s.get_random_person());
-        baza.insert({p->get_pesel(),p});
-    }
-    print_container(baza);
-    std::cout<<"WZORZEC /|\\ \n######################################################\n"; 
-
-    for (int i=0; i<argc-2; i+=3 )
+    Person s;
+try{
+    if((argc<3)||((argc-2)%3!=0))
     {
-        get_data(baza,std::stoi(argv[i+2]),std::stoi(argv[i+3]),std::stoi(argv[i+4]));
-    }    
-return 0;
+        std::cout<<"Za malo parametrow 1 + 3*N ( powinno być: "
+                 <<"$ ./projekt <ilosc osob do wygenerowania> <tryb sortowania 0-5>"
+                 <<"<pozycja od ktorej wypisywac> <ile pozycji wypisac> )\n";
+    }
+    else if(argc > 32)
+    {
+        std::cout<<"Za duzo parametrow!(max 31 + nazwa programu!)\n";
+    }
+    else if(std::stoi(argv[1])<1 || std::stoi(argv[1])>1000000)
+    {
+        std::cout<<"1 parametr poza zakresem <1 - 1.000.000>\n";
+    }
+    else
+    {
+        for( int i=0; i<std::stoi(argv[1]); ++i)
+        { 
+            std::shared_ptr<Person> p = std::make_shared<Person>(s.get_random_person());
+            baza.insert({p->get_pesel(),p});
+        }
+        
+        for (int i=0; i<argc-2; i+=3 )
+        {
+            if( std::stoi(argv[i+2])<0 || std::stoi(argv[i+2])>6 )
+            {
+                std::cout<<"Nieprawidlowy zakres type sortowania!(2 parametr) <0-6> ! \n";
+            }
+            else if ( std::stoi(argv[i+3])<1 || std::stoi(argv[i+3])>std::stoi(argv[1]) )
+            {
+                std::cout<<"Nieprawidlowy zakres start_position wyswietlania!(3 parametr) <od <1> do <wartosc 1 parametru>> ! \n";
+            }
+            else if
+            (
+                (    
+                  ( std::stoi(argv[1])   ==  std::stoi(argv[i+3]) ) && ( std::stoi(argv[i+4]) != 1  )  
+                )
+                ||
+                ( 
+                    std::stoi(argv[i+4])  >  std::stoi(argv[1])
+                )
+                ||
+                ( 
+                    std::stoi(argv[i+4])  <  0 
+                )
+                ||
+                (
+                   ( std::stoi(argv[i+4])  >  ( std::stoi(argv[1]) - std::stoi(argv[i+3]) ) +1) && ( std::stoi(argv[i+4]) != 1  ) 
+                ) 
+            ) 
+            {
+                std::cout<< "Nieprawidlowy zakres amount wyswietlania!(4 parametr) < zwykle przyjmuje zakes od <1> do <wartosc parametru 1>"
+                         << "ale jest zależne od logiki <3 parametru>> ! \n";
+            }
+            else
+            {
+                get_data( baza, std::stoi(argv[i+2]), std::stoi(argv[i+3]), std::stoi(argv[i+4]) );
+            }
+        }
+    }  
+} catch (const std::exception& e) { 
+    std::cout<< "Jako parametr wprowadzono litere zamiast cyfry!!"
+                                           << "Blad wyrzucila funkcja: " << e.what()<<"\n";
+} catch (...) { 
+    std::cout<< "Wyskoczyl inny wyjatek ...\n"; 
+}  
+
+return 0;   
 }
